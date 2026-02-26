e4cd68c8932646cdb552ca6c3602decacca8c051
Sebas271106 <juansebastiansuarezpava@gmail.com>
2026-02-26 11:11:20 -0500

    Ejercicio Dos: Product

--- app/src/main/java/com/humpty/maquetacionbasica/MainActivity.kt
+++ app/src/main/java/com/humpty/maquetacionbasica/MainActivity.kt
@@ -1,23 +1,35 @@
 package com.humpty.maquetacionbasica
 
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,27 +46,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun composeTutorial() {
     Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row() {
-            val painter = painterResource(id = R.drawable.jetpackcompose)
+            val painter = painterResource(id = R.drawable.jetpack)
             Image(
                 painter = painter,
                 contentDescription = null,
                 modifier = Modifier
+                    .padding(top = 30.dp)
                     .fillMaxWidth()
             )
         }
         Text(
-            text = "Jetpack Compose Tutorial",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(16.dp)
+            text = "Jetpack Compose Tutorial", fontSize = 24.sp, modifier = Modifier.padding(16.dp)
         )
         Text(
             text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, nd intuitive Kotlin APIS.",
@@ -68,4 +77,90 @@ fun composeTutorial() {
         )
     }
 
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun product() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Row() {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+            ) {
+                val painter = painterResource(id = R.drawable.pc)
+
+                Image(
+                    painter = painter, contentDescription = null, modifier = Modifier.fillMaxSize()
+                )
+
+                Icon(
+                    painter = painterResource(id = R.drawable.heartsvg),
+                    contentDescription = "Logo",
+                    tint = Color.Yellow,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(13.dp)
+
+                )
+            }
+        }
+        Text(
+            text = "Nombre Del Producto",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = "$99.99", fontSize = 28.sp, fontWeight = FontWeight.Bold, color = Color.Green
+        )
+        Text(
+            text = "Descripción del producto con detalles importantes, características y beneficios.",
+            modifier = Modifier.padding(top = 10.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(
+                8.dp,
+                Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    colorResource(id = R.color.Purple)
+                ),
+                modifier = Modifier.weight(1f),
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.pencilsvg),
+                    contentDescription = "Logo",
+                )
+                Text(
+                    text = "Editar", modifier = Modifier.padding(horizontal = 10.dp)
+                )
+            }
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red, // Color de fondo
+                ),
+                modifier = Modifier.weight(1f),
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.deletesvg),
+                    contentDescription = "Logo",
+                )
+                Text(
+                    text = "Eliminar", modifier = Modifier.padding(horizontal = 10.dp)
+                )
+            }
+        }
+    }
 }
